620a9b81e06d7ffee43bd288a61969194a7e3e78
Stanciu Mihai <misu_sergiu@yahoo.com>
2021-04-22 22:26:57 +0300

    Started work on Add Vendors and Add Items function

---
+++ ConsignmentShopUI/AddVendors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsignmentShopUI
+{
+    public partial class AddVendors : Form
+    {
+        public AddVendors()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ConsignmentShopUI/ConsignmentShop.Designer.cs
+++ ConsignmentShopUI/ConsignmentShop.Designer.cs
@@ -41,6 +41,8 @@ namespace ConsignmentShopUI
             this.vendorListbox = new System.Windows.Forms.ListBox();
             this.storePayoutLabel = new System.Windows.Forms.Label();
             this.storeProfitValue = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // headerText
@@ -160,11 +162,31 @@ namespace ConsignmentShopUI
             this.storeProfitValue.TabIndex = 11;
             this.storeProfitValue.Text = "$0.00";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(660, 498);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 50);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Add Vendors";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(768, 498);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 50);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Add Items";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // ConsignmentShop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(861, 560);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.storeProfitValue);
             this.Controls.Add(this.storePayoutLabel);
             this.Controls.Add(this.vendorListBoxLabel);
@@ -201,6 +223,8 @@ namespace ConsignmentShopUI
         private System.Windows.Forms.ListBox vendorListbox;
         private System.Windows.Forms.Label storePayoutLabel;
         private System.Windows.Forms.Label storeProfitValue;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
